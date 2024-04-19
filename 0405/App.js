8da0074ac465e8a477aa6f31850506b6ef755d4f
mooner1213 <162667655+mooner1213@users.noreply.github.com>
2024-04-19 21:07:26 +0900

    Add files via upload

---
+++ 0405/App.js
@@ -0,0 +1,16 @@
+import React from "react"; // 리액트를 구현할 수 있는 플러그인을 연결
+import Header from "./Header";
+import Footer from "./Footer";
+import Main from "./Main";
+// JS파일에 외부 파일을 불러오는 것이기 때문에 "import" 키워드를 사용한다.
+// 같은 JS파일은 확장자를 사용하지 않는다.
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ 0405/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Main(props) {
+  return (
+    <div>
+      <main>
+        <h1>안녕하세요! 메인입니다.</h1>
+      </main>
+    </div>
+  );
+}
+export default Main;
